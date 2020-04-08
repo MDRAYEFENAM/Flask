abb4d6b6b756b708f08a734e10cf168516ce8412
Md. Rayef Enam <rayefcse34@gmail.com>
2020-04-08 22:13:02 +0600

    Create Route
    create different route your as like as

---
+++ stringRoute.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello, Rayef!"
+    
+@app.route("/<string:name>")
+def hello(name):
+    return f"Hello, {name}!"
+    
+
+if __name__ == "__main__":
+    app.run()
